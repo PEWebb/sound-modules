f9ee7bad6b1de74991691922ec437baa3a9309d6
Alan Cheng <adcheng@mit.edu>
2017-01-27 17:09:46 -0500

    First major update to module example. Changes: single button; auto-playback. Not-debugged fully yet

--- modules/modules.ino
+++ modules/modules.ino
@@ -1,9 +1,24 @@
+//--------------------------------------------------------------
+// Teensy Sensor Module Test:
+// Using potentiometer and single button
+//
+// Info: This program acts as a test base for creating
+//       future sound modules.
+//       - Single button for recording (press to start/stop)
+//       - Switches to play if potentiometer reaches new value
+//       - Currently uses the "Flange" effect preset
+//--------------------------------------------------------------
+
+// Including Audioshield libraries
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 #include <Bounce.h>
+
+// Pitch playback (not implemented)
+#include "pitches.h"
 
 // GUItool: begin automatically generated code
 AudioPlaySdRaw           playSdRaw1;     //xy=293,302
@@ -18,21 +33,25 @@ AudioConnection          patchCord4(flange1, 0, i2s2, 1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=458,435
 // GUItool: end automatically generated code
 
-//   Record Button: pin 0 to GND
-//   Stop Button:   pin 1 to GND
-//   Play Button:   pin 2 to GND
+// For debugging reasons, the Teensy's LED is used:
+// HIGH: States 0,2 (Idle/playback)
+// LOW: States 1 (Recording)
+int ledPin = 13; // Will be off when recording
+bool ledState = HIGH;
 
+// Flange parameters
 #define FLANGE_DELAY_LENGTH (12*AUDIO_BLOCK_SAMPLES)
 int s_idx = 3*FLANGE_DELAY_LENGTH/4;
 int s_depth = FLANGE_DELAY_LENGTH/8;
 double s_freq = 0;
 short delayline[FLANGE_DELAY_LENGTH];
 
+// Record Button: pin 0 to GND
 // Bounce objects to easily and reliably read the buttons
+// 8 = 8 ms debounce time
 Bounce buttonRecord = Bounce(0, 8);
-Bounce buttonStop =   Bounce(1, 8);  // 8 = 8 ms debounce time
-Bounce buttonPlay =   Bounce(2, 8);
 
+// Mic settings
 //const int myInput = AUDIO_INPUT_LINEIN;
 const int myInput = AUDIO_INPUT_MIC;
 const int flexpin = 0; 
@@ -43,12 +62,69 @@ int mode = 0;  // 0=stopped, 1=recording, 2=playing
 // The file where data is recorded
 File frec;
 
+// Moving average sensor data array
+double data[3];
+double diffThresh = 10; // Threshold for autoplay
+//-------------------------[Moving Average Functions]-----------------------
+/* This function initializes an array for moving average
+ *  @Param: double array_n[]: moving average array
+ *  @Param: double val: initial value
+ *  @Output: Returns nothing; but updates array
+ */
+void init_array(double array_n[], int val){
+  for(int i = 0; i < sizeof(array_n); i++){
+    array_n[i] = val;  
+  }
+}
+
+/* This function calculates the difference between two sequential timesteps
+ *  @Param: double array_n[]: moving average array
+ *  @Output: Returns the difference between the moving average of two
+ *           sequential timesteps
+ */
+double get_array_diff(double array_n[]){
+  double total = 0;
+  for(int i = 1; i < sizeof(array_n); i++){
+    total += array_n[i];
+  }
+  double avg_1 = total/(sizeof(array_n)-1);
+  
+  total = 0;
+  for(int i = 0; i < sizeof(array_n)-1; i++){
+    total += array_n[i];
+  }
+  
+  double avg_2 = total/(sizeof(array_n)-1);
+  //Serial.println(avg_1);
+  //Serial.println(avg_2);
+  return avg_2 - avg_1;
+}
+
+/* This function populates the moving average array with new data
+ *  @Param: double array_n[]: moving average array
+ *  @Param: double val: initial value
+ *  @Output: Returns nothing; but updates array
+ */
+void update_array(double array_n[], double val) {
+  for(int i = 1; i < sizeof(array_n); i++){
+    array_n[i] = array_n[i-1];
+  }
+  array_n[0] = val;
+}
+
+//------------------------------[Initialization]----------------------------
+/* This functions initializes the Teensy module
+ *  @Output: Returns nothing
+ */
 void setup() {
-  // Configure the pushbutton pins
+  // Set serial port baud rate
   Serial.begin(9600);
+  
+  // Configure the record pushbutton pin
   pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
+
+  // Configure the LED on the Teensy
+  pinMode(ledPin, OUTPUT);
 
   // Audio connections require memory, and the record queue
   // uses this memory to buffer incoming audio.
@@ -59,7 +135,13 @@ void setup() {
   sgtl5000_1.inputSelect(myInput);
   sgtl5000_1.volume(1);
 
+  // Initialize flange
   flange1.begin(delayline, FLANGE_DELAY_LENGTH, s_idx, s_depth, s_freq);
+
+  // Initialize data array
+  int flexposition = analogRead(flexpin);
+  double mappedFlex = map(flexposition, 8, 10, 0, 800)/1000.0;
+  init_array(data, mappedFlex);
 
   // Initialize the SD card
   SPI.setMOSI(7);
@@ -73,60 +155,80 @@ void setup() {
   }
 }
 
-
+//-----------------------------------[Main]---------------------------------
+/* This is the main function
+ *  @Output: Returns nothing
+ */
 void loop() {
-  // First, read the buttons
-  buttonRecord.update();
-  buttonStop.update();
-  buttonPlay.update();
 
+  // Define variables to store data
   int flexposition;
   double mappedFlex;   
 
+  // Read data from potentiometer
   flexposition = analogRead(flexpin);
   //delayTime = map(flexposition, 1023, 930, 100, 0)/100.0;
   //sgtl5000_1.volume(delayTime);
   mappedFlex = map(flexposition, 8, 10, 0, 800)/1000.0;
-  flange1.voices(s_idx, s_depth, mappedFlex);
 
+  // Apply flange
+  flange1.voices(s_idx, s_depth, mappedFlex);
+  
+  // Debugging print statements
   Serial.print("sensor: ");
   Serial.print(flexposition);
   Serial.print("  ");
   Serial.println(mappedFlex);
+
+  // Update data array
+  update_array(data, mappedFlex);
+
+  // Get difference from this and last time step
+  double diff = abs(get_array_diff(data));
   
+  // when using a microphone, continuously adjust gain
+  if (myInput == AUDIO_INPUT_MIC) adjustMicLevel();
+  
+  // First, read the buttons
+  buttonRecord.update();
 
-  // Respond to button presses
-  if (buttonRecord.fallingEdge()) {
-    Serial.println("Record Button Press");
-    if (mode == 2) stopPlaying();
-    if (mode == 0) startRecording();
-  }
-  if (buttonStop.fallingEdge()) {
-    Serial.println("Stop Button Press");
-    if (mode == 1) stopRecording();
-    if (mode == 2) stopPlaying();
-  }
-
-  if (buttonPlay.fallingEdge()) {
-    Serial.println("Play Button Press");
+  // If potentiometer is changed enough
+  if (diff > diffThresh) {
+    
+    Serial.println("Play");
     if (mode == 1) stopRecording();
     if (mode == 0) startPlaying();
   }
+  
+  // Respond to button presses
+  // If record button is pressed
+  else if (buttonRecord.fallingEdge()) {
+    Serial.println("Record Button Press");
 
+    // If currently playing something: stop
+    if (mode == 2) stopPlaying();
+
+    // If currently recording: stop
+    if (mode == 1) stopRecording();
+
+    // Else (idle): start recording
+    else if (mode == 0) startRecording();
+  }
+  
   // If its playing or recording, carry on...
-  if (mode == 1) {
+  else if (mode == 1) {
     continueRecording();
   }
-  if (mode == 2) {
+  else if (mode == 2) {
     continuePlaying();
   }
 
-  // when using a microphone, continuously adjust gain
-  if (myInput == AUDIO_INPUT_MIC) adjustMicLevel();
+  // Update LED state
+  digitalWrite(ledPin, ledState); // updates LED
 
 }
 
-
+//-------------------------------[Record/Play]------------------------------
 void startRecording() {
   Serial.println("startRecording");
   if (SD.exists("RECORD.RAW")) {
@@ -139,6 +241,8 @@ void startRecording() {
   if (frec) {
     queue1.begin();
     mode = 1;
+
+    ledState = HIGH;
   }
 }
 
@@ -180,6 +284,8 @@ void stopRecording() {
       queue1.freeBuffer();
     }
     frec.close();
+
+    ledState = HIGH;
   }
   mode = 0;
 }
