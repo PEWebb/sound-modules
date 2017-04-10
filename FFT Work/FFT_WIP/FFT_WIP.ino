718eff32079e32431f1883ada3392b1df8cff1d7
Alan Cheng <adcheng@mit.edu>
2017-04-10 02:51:25 -0400

    started work on cfft lib file for sound processing

--- FFT Work/FFT_WIP/FFT_WIP.ino
+++ FFT Work/FFT_WIP/FFT_WIP.ino
@@ -1,96 +1,73 @@
-// Adapted from: https://forum.pjrc.com/archive/index.php/t-27832.html
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Bounce.h>
 
-#include "arm_math.h"
+// GUItool: begin automatically generated code
+AudioInputI2S            i2sIn;           //xy=173.1999969482422,160.1999969482422
+AudioPlaySdRaw           playSdRawFile;     //xy=179.11112213134766,311.3333215713501
+AudioOutputI2S           i2sOut;           //xy=521.8666572570801,303.08887481689453
+AudioAnalyzePeak         micPeak;          //xy=525.7777366638184,100.2222146987915
+AudioRecordQueue         micQueue;         //xy=529.4222183227539,154.1999912261963
+AudioConnection          patchCord1(i2sIn, 0, micQueue, 0);
+AudioConnection          patchCord2(i2sIn, 0, micPeak, 0);
+AudioConnection          patchCord3(playSdRawFile, flangeEffect);
+AudioConnection          patchCord4(flangeEffect, 0, i2sOut, 1);
+AudioConnection          patchCord5(flangeEffect, 0, i2sOut, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=408,471.3333148956299
+// GUItool: end automatically generated code
 
-#define FFTSIZE 256
+// Flange parameters
+#define FLANGE_DELAY_LENGTH (12*AUDIO_BLOCK_SAMPLES)
+int s_idx = 3*FLANGE_DELAY_LENGTH/4;
+int s_depth = FLANGE_DELAY_LENGTH/8;
+double s_freq = 0.5;
+short delayline[FLANGE_DELAY_LENGTH];
 
-int16_t s1[FFTSIZE], s2[FFTSIZE];
-int16_t fft_buf[FFTSIZE * 2];
-int32_t ifft_buf[FFTSIZE * 2];
-arm_cfft_radix4_instance_q15 fft_inst;
-arm_cfft_radix4_instance_q31 ifft_inst;
+const int flexpin = 2;
+
+// Bounce objects to easily and reliably read the buttons
+Bounce buttonRecord = Bounce(0, 8);
+Bounce buttonPlay   = Bounce(1, 8);  // 8 = 8 ms debounce time
+
+// Which is the input medium
+const int myInput = AUDIO_INPUT_LINEIN;
+//const int myInput = AUDIO_INPUT_MIC;
+
+// Remember which mode we're doing
+int mode = 0;  // 0=stopped, 1=recording, 2=playing
+
+// The file where data is recorded
+File fRec;
 
 void setup() {
-  Serial.begin(9600);
-  arm_cfft_radix4_init_q15(&fft_inst, FFTSIZE, 0, 1);
-  arm_cfft_radix4_init_q31(&ifft_inst, FFTSIZE, 1, 1);
+  // Configure the pushbutton pins
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+
+  // Audio connections require memory, and the record queue
+  // uses this memory to buffer incoming audio.
+  AudioMemory(60);
+
+  // Enable the audio shield, select input, and enable output
+  sgtl5000_1.enable();
+  sgtl5000_1.inputSelect(myInput);
+  sgtl5000_1.volume(1.0);
+  
+  // Initialize the SD card
+  SPI.setMOSI(7);
+  SPI.setSCK(14);
+  if (!(SD.begin(10))) {
+    // stop here if no SD card, but print a message
+    while (1) {
+      Serial.println("Unable to access the SD card");
+      delay(500);
+    }
+  }
 }
 
 void loop() {
-  // generate two signals with random data, s2 is delayed s1
-  unsigned offset = random(FFTSIZE / 4);
-  for (unsigned i = 0; i < FFTSIZE + offset; i++) {
-    int16_t val = ((int16_t) random(65536));
-    if (i < FFTSIZE)
-    s1[i] = val;
-    if (i > offset)
-    s2[i - offset] = val;
-  }
-
-  // Copy samples interleaved to FFT buffer s1 in real part, s2 in complex part
-  // ie. fftbuf = s1 + j * s2
-  for (int i = 0; i < FFTSIZE; i++)
-    ((int32_t *)fft_buf)[i] = (s2[i] << 16) | s1[i];
     
-  elapsedMicros us; // start timing
-  
-  // Perform FFT
-  arm_cfft_radix4_q15(&fft_inst, fft_buf);
-
-  // Untangle fft of both signals and calculate conj(fft(s1)) * fft(s2)
-  // Treat DC point special
-  int16_t fs1_re, fs1_im, fs2_re, fs2_im;
-  fs1_re = fft_buf[0]; // fs1_im = 0
-  fs2_re = fft_buf[0]; // fs2_im = 0
-  ifft_buf[0] = fs1_re * fs2_re;
-  ifft_buf[1] = 0;
-  // Then handle most other points
-  int16_t *fft_fwd = fft_buf + 2;
-  int16_t *fft_rev = fft_buf + 2 * (FFTSIZE - 1);
-  for (size_t i = 2; i < FFTSIZE; i += 2, fft_fwd += 2, fft_rev -= 2) {
-    fs1_re = (fft_fwd[0] + fft_rev[0]) >> 1;
-    fs1_im = (fft_fwd[1] - fft_rev[1]) >> 1;
-    fs2_re = (fft_rev[1] + fft_fwd[1]) >> 1;
-    fs2_im = (fft_rev[0] - fft_fwd[0]) >> 1;
-    // calculate conj(fft(s1)) * fft(s2)
-    int32_t cfs1_fs2_re = (fs1_re * fs2_re + fs1_im * fs2_im);
-    int32_t cfs1_fs2_im = (fs1_re * fs2_im - fs1_im * fs2_re);
-    ifft_buf[i] = ifft_buf[2 * FFTSIZE - i] = cfs1_fs2_re;
-    ifft_buf[i + 1] = cfs1_fs2_im;
-    ifft_buf[2 * FFTSIZE - i + 1] = -cfs1_fs2_im;
-  }
-  // Finally handle Nyquist point
-  fs1_re = fft_fwd[0]; // fs1_im = 0
-  fs2_re = fft_fwd[1]; // fs2_im = 0
-  ifft_buf[FFTSIZE] = fs1_re * fs2_re;
-  ifft_buf[FFTSIZE + 1] = 0;
-  
-  // Perform inverse FFT
-  arm_cfft_radix4_q31(&ifft_inst, ifft_buf);
-  
-  uint32_t duration = us; // end of timing
-  
-  // The output data is in every second element of ifft_buf
-  
-  // As an example, find the cross-cor peak
-  int peak_offset = 0;
-  int32_t peak_value = ifft_buf[0];
-  for (size_t i = 1, j = 2; i < FFTSIZE; i++, j += 2) {
-    if (ifft_buf[j] > peak_value) {
-      peak_value = ifft_buf[j];
-      peak_offset = i;
-    }
-  }
-  if (peak_offset > FFTSIZE / 2)
-  peak_offset -= FFTSIZE;
-  
-  Serial.print("Generated offset: ");
-  Serial.println(offset); 
-  Serial.print("Calculated offset: ");
-  Serial.println(-peak_offset); // delay implies negative
-  Serial.print("It took ");
-  Serial.print(duration);
-  Serial.println(" us\n");
-  
-  delay(500);
 }
